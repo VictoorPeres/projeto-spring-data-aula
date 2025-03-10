5676407a575731532b1b2641de23818626cae05e
Victor Oliveira <victor.oliveira@fenixsoft.local>
2025-03-10 17:08:51 -0400

    Criação de consultas com a anotação @Query

--- src/main/java/br/com/atom/dao/InterfaceSpringDataUser.java
+++ src/main/java/br/com/atom/dao/InterfaceSpringDataUser.java
@@ -1,10 +1,32 @@
 package br.com.atom.dao;
 
 import br.com.atom.model.UsuarioSpringData;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long> {
 
+    @Query(value = "select u from UsuarioSpringData u where u.nome like %?1%")
+    public List<UsuarioSpringData> buscaPorNome(String nome);
+
+    @Transactional(readOnly = true) /* ReadOnly define que o metodo sera APENAS LEITURA */
+    @Query(value = "select u from UsuarioSpringData u where u.nome = :paramnome")
+    public UsuarioSpringData buscaPorNomeParam(@Param("paramnome") String paramnome);
+
+    @Modifying
+    @Transactional
+    @Query(value = "delete from UsuarioSpringData u where u.nome = ?1")
+    public void deletePorNome(String nome);
+
+    @Modifying
+    @Transactional
+    @Query(value = "update UsuarioSpringData u set u.email = ?1 where u.nome = ?2")
+    public void updateEmailPorNome(String email, String nome);
 }
--- src/test/java/AppSpringDataTest.java
+++ src/test/java/AppSpringDataTest.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import java.util.List;
+import java.util.Optional;
+
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 
@@ -15,7 +18,7 @@ public class AppSpringDataTest {
     private InterfaceSpringDataUser interfaceSpringDataUser;
 
     @Test
-    public void testInsert() {
+    public void testeInsert() {
         UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
         usuarioSpringData.setEmail("javaavancado@javaavancado.com");
         usuarioSpringData.setIdade(31);
@@ -24,5 +27,79 @@ public class AppSpringDataTest {
         usuarioSpringData.setNome("Victor Oliveira");
 
         interfaceSpringDataUser.save(usuarioSpringData);
+    }
+
+    @Test
+    public void testeConsulta(){
+        Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+        System.out.println("Nome: " + usuarioSpringData.get().getNome());
+        System.out.println("Email: " + usuarioSpringData.get().getEmail());
+        System.out.println("Login: " + usuarioSpringData.get().getLogin());
+        System.out.println("Idade: " + usuarioSpringData.get().getIdade());
+    }
+
+    @Test
+    public void testeConsultaAll(){
+        Iterable<UsuarioSpringData> lista = interfaceSpringDataUser.findAll();
+
+        for (UsuarioSpringData usuarioSpringData : lista) {
+            System.out.println("-----------------------------------------------");
+            System.out.println("Nome: " + usuarioSpringData.getNome());
+            System.out.println("Email: " + usuarioSpringData.getEmail());
+            System.out.println("Login: " + usuarioSpringData.getLogin());
+            System.out.println("Idade: " + usuarioSpringData.getIdade());
+        }
+    }
+
+    @Test
+    public void testeUpdate(){
+        Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+        UsuarioSpringData data = usuarioSpringData.get();
+
+        data.setNome("Alexandre Peres");
+
+        interfaceSpringDataUser.save(data);
+    }
+
+    @Test
+    public void testeDelete(){
+        Optional<UsuarioSpringData> usuarioSpringData = interfaceSpringDataUser.findById(1L);
+        UsuarioSpringData data = usuarioSpringData.get();
+        interfaceSpringDataUser.delete(data);
+
+    }
+
+    @Test
+    public void testeDeleteById(){
+        interfaceSpringDataUser.deleteById(1L);
+    }
+
+    @Test
+    public void testeConsultaNome(){
+        List<UsuarioSpringData> lista = interfaceSpringDataUser.buscaPorNome("Victor");
+
+        for (UsuarioSpringData usuarioSpringData : lista) {
+            System.out.println("----------------------------------------");
+            System.out.println("Id: " + usuarioSpringData.getId());
+            System.out.println("Nome: " + usuarioSpringData.getNome());
+
+        }
+    }
+
+    @Test
+    public void testeConsultaNomeParam(){
+        UsuarioSpringData usuarioSpringData = interfaceSpringDataUser.buscaPorNomeParam("Victor Oliveira");
+        System.out.println("Nome: " + usuarioSpringData.getId());
+        System.out.println("Nome: " + usuarioSpringData.getNome());
+    }
+
+    @Test
+    public void testeDeletePorNome(){
+        interfaceSpringDataUser.deletePorNome("Victor Oliveira");
+    }
+
+    @Test
+    public void testeUpdateEmailPorNome(){
+        interfaceSpringDataUser.updateEmailPorNome("victor@gmail.com", "Alexandre Peres");
     }
 }
